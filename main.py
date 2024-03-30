77f519d816911921dc7ac958cf20f1c43b03865c
Zarya Foju <zaryafoju@gmail.com>
2024-03-30 14:56:06 -0500

    Adding basic menu, setting up functions

---
+++ main.py
@@ -0,0 +1,40 @@
+
+user = input('Enter your name: ')
+password = input('Enter the password for your account')
+#Possible username and password function before access to menu, needs mysql
+
+print('Welcome,', user)
+
+print('')
+print('========================')
+print('Menu: Banking Account')
+
+print('1. Create Account')
+print('2. Delete Account')
+print('3. Check Balance')
+print('4. Deposit')
+print('5. Withdraw')
+print('6. Modify Account')
+print('========================')
+
+menu_option = int(input('Please select one of the options for your account: '))
+
+#Main functions for account
+
+def create_account():
+    pass 
+
+def delete_account():
+    pass 
+
+def check_balance():
+    pass 
+
+def deposit():
+    pass 
+
+def withdraw():
+    pass 
+
+def modify_account():
+    pass 
